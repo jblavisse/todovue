4963fa0048242191e83332602b2a943cdb2578f4
JB Lavisse <jean-baptiste@jblavisse.fr>
2021-02-18 15:56:59 +0100

    Add Task component with form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <input v-model="taskUser" />
+    </form>
+    {{taskUser}}
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <Task :title="task.title" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Task from './components/Task';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Task
+  },
+  data: function() {
+    return {
+    tasks:  [
+      {id: 1, title: "Faire à manger", completed: false},
+      {id: 2, title: "Faire la vaiselle", completed: true},
+      {id: 3, title: "Dormir", completed:false}
+    ],
+    taskUser: ""
+    }
+  } 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="task">
+        {{title}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style>
+.task {
+    color: red;
+}
+</style>
